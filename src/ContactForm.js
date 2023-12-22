632668f295263f222faee798dcc2ff5d4eee6189
Niharika Sable <niharikasable@gmail.com>
2023-12-22 15:50:20 +0530

    initialize commit

---
+++ src/ContactForm.js
@@ -0,0 +1,44 @@
+// src/ContactForm.js
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: Send the form data to the server for email sending
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Message:
+        <textarea name="message" value={formData.message} onChange={handleChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
---
+++ src/app.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contact Form App</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
