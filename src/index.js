4f0075ff45322189d495e0b59994ce8cbea932a2
Niharika Sable <niharikasable@gmail.com>
2023-12-22 13:48:00 +0530

    initialize commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 5000;
 const sendMail = require("../controllers/sendMail");
+
+app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/",(req,res)=>{
     res.send("I am a server");
